2867957366fb6fba1cf61e3be297945e8dedf74d
Valeria Aleshko <aleshko.valery@gmail.com>
2016-11-25 00:02:42 +0300

    I've tryed(((

--- Sockets/client.py
+++ Sockets/client.py
@@ -2,17 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import threading
 
+def answer(interval):
+	while True:
+		dataout = sock.recv(1024)
+		if dataout:
+			print('2: ' + dataout.decode("utf-8"))
+	
+def request(interval):
+	while True:
+		datain = input('1: ')
+		if datain == 'q' or datain == 'end':
+			return
+		sock.send(datain.encode("utf-8"))
+
+
+
+		
 sock = socket.socket()
-sock.connect(('localhost', 6666))
-data = ' '
-while True:
-	data = input()
-	if data == 'end':
-		break
-	sock.send(data.encode("utf-8"))	
-	data = sock.recv(1024)
-	print(data.decode("utf-8"))
+sock.connect(('localhost', 6668))
+#data = ' '
+print ('Start')
+
+ta = threading.Thread(target=answer, args=(1,))
+tr = threading.Thread(target=request, args=(1,))
+
+ta.start()
+tr.start()
+
 sock.close()
 
 input('Press any button...')
--- Sockets/server.py
+++ Sockets/server.py
@@ -4,16 +4,26 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 6666))
-sock.listen(1)
-conn, addr = sock.accept()
+sock.bind(('', 6668))
+sock.listen(2)
+conn1, addr1 = sock.accept()
+conn2, addr2 = sock.accept()
 
-print ('connected:', addr)
+print ('1 - connected: ', addr1)
+print ('2 - connected: ', addr2)
 
 while True:
-    data = conn.recv(1024)
-    if data == b'end':
-        break
-    conn.send(data.upper())
+	data1 = conn1.recv(1024)
+	data2 = conn2.recv(1024)
+	
+	if data1 == b'q':
+		conn2.send('1st client is not here'.encode("utf-8"))
+	if data2 == b'q':
+		conn1.send('2nd client is not here'.encode("utf-8"))
+	
+	if (data1 == b'end') or (data2 == b'end') :
+		break
+	conn1.send(data2)
+	conn1.send(data1)
 
 conn.close()
