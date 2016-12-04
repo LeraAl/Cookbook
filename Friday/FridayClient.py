eff878c408bc4b3088bab40c29f27c65ba9b520c
Valeria Aleshko <aleshko.valery@gmail.com>
2016-12-04 20:44:34 +0300

    Fix russian i/o

--- Friday/FridayClient.py
+++ Friday/FridayClient.py
@@ -2,19 +2,20 @@
 import http.client as httplib
 
 def printText(txt):
-    lines = txt.split('\n')
+    lines = txt.decode('utf-8').split('\n')
     for line in lines:
         print (line.strip())
 
 		
-server = input('Input the server:\n')		
+server = input('Input the server:\n')	
+server = server + '.ngrok.io'	
 httpServ = httplib.HTTPConnection(server)
 httpServ.connect()
 
 request = input('input your request: \n')
 request = 'user:' + request
 print (request)
-httpServ.request('POST', '/index', request)
+httpServ.request('POST', '/index', request.encode('utf-8'))
 
 response = httpServ.getresponse()
 if response.status == httplib.OK:
